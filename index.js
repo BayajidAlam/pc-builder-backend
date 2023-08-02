c1803765144f0f5e65755301b62bfc88391d088e
BayajidAlam <bayajidalam2001@gmail.com>
2023-08-02 14:41:34 +0600

    produt details api implemanted

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 // middleware
@@ -42,6 +42,7 @@ async function run() {
       res.send(result);
     });
 
+    // load data based on component name 
     app.get('/cpu', async(req,res)=>{
       const pcData = myCollection.find({
         category: "CPU / Processor"
@@ -84,7 +85,21 @@ async function run() {
       const result = await pcData.toArray();
       res.send(result)
     })
+    app.get('/others', async(req,res)=>{
+      const pcData = myCollection.find({
+        category: "Others"
+      })
+      const result = await pcData.toArray();
+      res.send(result)
+    })
 
+    // get a specific product details 
+    app.get('/product/:id',async(req,res)=>{
+      const id = req.params.id;
+      console.log(id);
+      const pcData = await myCollection.findOne({_id:new ObjectId(id)})
+      res.send(pcData)
+    })
 
     app.listen(port, () => {
       console.log(`App listening on port ${port}`);
