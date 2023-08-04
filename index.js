3cc2f0fdd6b9c392538030ed7eaae7e91685138a
BayajidAlam <bayajidalam2001@gmail.com>
2023-08-04 17:07:18 +0600

    last update for carry

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
-const cors = require('cors');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-require('dotenv').config();
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
 
 // middleware
 app.use(cors());
@@ -25,81 +25,91 @@ async function run() {
   try {
     // Connect the MongoDB client before registering route handlers
     await client.connect();
-    console.log('Connected to MongoDB');
+    console.log("Connected to MongoDB");
 
-    const myCollection = client.db('pcBuilder').collection('components');
+    // collections 
+    const myCollection = client.db("pcBuilder").collection("components");
+    const builderComponents = client.db('pcBuilder').collection('builderCompo')
 
     // general/initial api
-    app.get('/', (req, res) => {
-      res.send('hello world');
+    app.get("/", (req, res) => {
+      res.send("hello world");
     });
 
     // get all components data
-    app.get('/pc', async (req, res) => {
+    app.get("/pc", async (req, res) => {
       const query = {};
       const options = myCollection.find(query);
       const result = await options.toArray();
       res.send(result);
     });
 
-    // load data based on component name 
-    app.get('/cpu', async(req,res)=>{
+    // load data based on component name
+    app.get("/cpu", async (req, res) => {
       const pcData = myCollection.find({
-        category: "CPU / Processor"
-      })
+        category: "CPU / Processor",
+      });
       const result = await pcData.toArray();
-      res.send(result)
-    })
-    app.get('/motherboard', async(req,res)=>{
+      res.send(result);
+    });
+    app.get("/motherboard", async (req, res) => {
       const pcData = myCollection.find({
-        category: "Motherboard"
-      })
+        category: "Motherboard",
+      });
       const result = await pcData.toArray();
-      res.send(result)
-    })
-    app.get('/ram', async(req,res)=>{
+      res.send(result);
+    });
+    app.get("/ram", async (req, res) => {
       const pcData = myCollection.find({
-        category: "RAM"
-      })
+        category: "RAM",
+      });
       const result = await pcData.toArray();
-      res.send(result)
-    })
-    app.get('/psu', async(req,res)=>{
+      res.send(result);
+    });
+    app.get("/psu", async (req, res) => {
       const pcData = myCollection.find({
-        category: "PSU"
-      })
+        category: "PSU",
+      });
       const result = await pcData.toArray();
-      res.send(result)
-    })
-    app.get('/storage', async(req,res)=>{
+      res.send(result);
+    });
+    app.get("/storage", async (req, res) => {
       const pcData = myCollection.find({
-        category: "Storage Device"
-      })
+        category: "Storage Device",
+      });
       const result = await pcData.toArray();
-      res.send(result)
-    })
-    app.get('/monitor', async(req,res)=>{
+      res.send(result);
+    });
+    app.get("/monitor", async (req, res) => {
       const pcData = myCollection.find({
-        category: "Monitor"
-      })
+        category: "Monitor",
+      });
       const result = await pcData.toArray();
-      res.send(result)
-    })
-    app.get('/others', async(req,res)=>{
+      res.send(result);
+    });
+    app.get("/others", async (req, res) => {
       const pcData = myCollection.find({
-        category: "Others"
-      })
+        category: "Others",
+      });
       const result = await pcData.toArray();
-      res.send(result)
-    })
+      res.send(result);
+    });
 
-    // get a specific product details 
-    app.get('/product/:id',async(req,res)=>{
+    // get a specific product details
+    app.get("/product/:id", async (req, res) => {
       const id = req.params.id;
       console.log(id);
-      const pcData = await myCollection.findOne({_id:new ObjectId(id)})
-      res.send(pcData)
-    })
+      const pcData = await myCollection.findOne({ _id: new ObjectId(id) });
+      res.send(pcData);
+    });
+
+       // get all builder components data
+       app.get("/builder-data", async (req, res) => {
+        const query = {};
+        const options = builderComponents.find(query);
+        const result = await options.toArray();
+        res.send(result);
+      });
 
     app.listen(port, () => {
       console.log(`App listening on port ${port}`);
